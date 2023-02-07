ce4e959d91b71345395f8966f7b5d6c5e25de5a0
just2102 <thesixteensaltines@gmail.com>
2023-02-07 10:04:58 +0900

    allow cors

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db")
 const dotenv = require("dotenv").config();
 const app = express();
+// allow cors 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 connectDB()
 
